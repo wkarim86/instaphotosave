9d9ac96afeb367998f2c826b9e2b6e50d4f2365c
wkarim86 <wkarim86@gmail.com>
2018-03-09 17:32:57 +0500

    My first IOS swift application to download Instagram pictures

---
+++ InstaPhotoSave/CustomCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomCell.swift
+//  InstaPhotoSave
+//
+//  Created by Waqas Karim on 21/02/2018.
+//  Copyright © 2018 Waqas Karim. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    var instaUrl: URL?
+    
+    @IBOutlet weak var downloadBtn: UIButton!
+    
+    @IBOutlet weak var openUrlBtn: UIButton!
+    
+    @IBAction func downloadAction(_ sender: UIButton) {
+        //download image into photo library
+        print("DownloadAction: \(instaUrl)")
+    }
+
+    @IBAction func openUrlAction(_ sender: UIButton) {
+        //open url on safari
+    }
+    
+}
---
+++ InstaPhotoSave/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  InstaPhotoSave
+//
+//  Created by Waqas Karim on 21/02/2018.
+//  Copyright © 2018 Waqas Karim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var clipboardUrl: URL?
+    var imageArray = [URL]()
+    let loader: UIActivityIndicatorView = UIActivityIndicatorView.init(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+    var selectedIndex: Int = Int()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(getUrl), name: Notification.Name("getUrl"), object: nil)
+        
+        //Setup Activity Indicator
+        loader.center = view.center
+        loader.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        loader.backgroundColor = UIColor.darkGray.withAlphaComponent(0.95)
+        self.view.addSubview(loader)
+        getUrl()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc public func getUrl(){
+        showLoader()
+        //assign new url from clipboard when app comes in foreground
+        clipboardUrl = UIPasteboard.general.url
+        imageArray.append(clipboardUrl!)
+        print("getUrl: \(imageArray)")
+        self.collectionView.reloadData()
+        hideLoader()
+    }
+    
+    func storeImage(index: Int){
+        //download and store image in photo library
+//        let alert = UIAlertController(title: "Alert", message: "URL: (/imageArray[index])", preferredStyle: UIAlertControllerStyle.alert)
+//        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+//        self.present(alert, animated: true, completion: nil)
+        showLoader()
+        print("image url:\(imageArray[selectedIndex])")
+        let imgData = try? Data(contentsOf: imageArray[selectedIndex])
+        let imagetoSave = UIImage(data: imgData!)
+        UIImageWriteToSavedPhotosAlbum(imagetoSave!, nil, nil, nil)
+        hideLoader()
+        
+    }
+    
+    
+ 
+    
+    func showLoader(){
+        loader.startAnimating()
+    }
+    
+    func hideLoader(){
+        loader.stopAnimating()
+    }
+
+
+}
+
+extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "thumbnailView", for: indexPath) as! CustomCell
+        let imageData = try? Data(contentsOf: imageArray[indexPath.row])
+        cell.thumbnail.image = UIImage(data: imageData!)
+        cell.instaUrl = imageArray[indexPath.row]
+        //cell.downloadBtn.addTarget(self, action: #selector(storeImage(index:indexPath.row)), for: UIControlEvents.touchUpInside)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedIndex = indexPath.row
+        print(imageArray[indexPath.row])
+    }
+    
+    
+}
+
